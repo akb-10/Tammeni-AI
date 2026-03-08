d56c1cf4344c6d7cfb18c776177a9157953d9fbd
akb-10 <baslimanabdbas@gmail.com>
2026-03-08 03:36:21 +0300

    Change API URL to production endpoint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentLang = 'ar';
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://tammeni-ai.onrender.com';
 
 // العناصر الأساسية
 const chatMessages = document.getElementById('chatMessages');
